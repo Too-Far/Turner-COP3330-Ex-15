9901b65516ba8c4ce18bf8f4d17d0aad15b86359
Ryan <rdturner40@gmail.com>
2021-08-31 14:04:14 -0400

    Initial MVP

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -4,10 +4,34 @@
  */
 package org.example;
 
-public class App 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Objects;
+
+public class App
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void getInput() throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String pwAttempt;
+        String password = "abc123";
+
+        while (true)
+        {
+            System.out.println("What is the password? ");
+            pwAttempt = reader.readLine();
+            if (Objects.equals(pwAttempt, password))
+            {
+                System.out.println("Welcome!");
+                break;
+            } else
+            {
+                System.out.println("I don't know you.");
+            }
+        }
+    }
+
+    public static void main( String[] args ) throws IOException {
+        getInput();
     }
 }
